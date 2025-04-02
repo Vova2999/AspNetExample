f47bf93b8bea3d063eccde6a76259db6c470d6eb
Vova2999 <Vova2999@gmail.com>
2025-04-02 21:50:59 +0400

    Add ping

---
+++ AspNetExample/Controllers/Api/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetExample.Controllers.Api;
+
+[AllowAnonymous]
+[ApiController]
+[Route("api/ping")]
+public class PingController : ControllerBase
+{
+    [HttpGet]
+    public void Get()
+    {
+    }
+}
