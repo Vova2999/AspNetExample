deae2f04f3c68604435ac6744601356bf4597d5a
Vova2999 <Vova2999@gmail.com>
2025-05-15 20:18:47 +0400

    Replace js code for change SortBy and Page

--- AspNetExample/Extensions/HtmlHelperExtensions.cs
+++ AspNetExample/Extensions/HtmlHelperExtensions.cs
@@ -4,6 +4,8 @@ using AspNetExample.Common.Extensions;
 using AspNetExample.Models;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
 
 namespace AspNetExample.Extensions;
 
@@ -11,12 +13,35 @@ public static class HtmlHelperExtensions
 {
     public static HtmlString DisplaySortColumnTitle<TModel, TResult>(
         this IHtmlHelper<TModel> htmlHelper,
+        HttpContext context,
         string? currentSortBy,
         string sortPropertyName,
         Expression<Func<TModel, TResult>> expression,
         bool isDefault = false)
     {
-        var stringBuilder = new StringBuilder();
+        (currentSortBy, var sortDescPropertyName, var nextSortPropertyName) =
+            CreateSortPropertyNames(currentSortBy, sortPropertyName, isDefault);
+
+        var nextUrl = CreateUrlWithChangedSort(context, nextSortPropertyName);
+
+        var stringBuilder = new StringBuilder()
+            .Append("<a href=\"")
+            .Append(nextUrl)
+            .Append("\" class=\"link-dark\">")
+            .Append(htmlHelper.DisplayNameFor(expression))
+            .Append("</a>");
+
+        if (currentSortBy == sortPropertyName)
+            stringBuilder.Append(" <i class=\"fa fa-sort-asc\" style=\"vertical-align: middle; margin-bottom: -5px;\"></i>");
+        else if (currentSortBy == sortDescPropertyName)
+            stringBuilder.Append(" <i class=\"fa fa-sort-desc\" style=\"vertical-align: middle; margin-top: -5px;\"></i>");
+
+        return new HtmlString(stringBuilder.ToString());
+    }
+
+    private static (string? CurrentSortBy, string SortDescPropertyName, string NextSortPropertyName)
+        CreateSortPropertyNames(string? currentSortBy, string sortPropertyName, bool isDefault)
+    {
         var sortDescPropertyName = $"{sortPropertyName}{Constants.DescSuffix}";
 
         if (currentSortBy.IsNullOrEmpty() && isDefault)
@@ -29,23 +54,26 @@ public static class HtmlHelperExtensions
                     ? string.Empty
                     : sortPropertyName;
 
-        stringBuilder
-            .Append("<a onclick=\"changeSortBy('")
-            .Append(nextSortPropertyName)
-            .Append("');\" class=\"link-dark\">")
-            .Append(htmlHelper.DisplayNameFor(expression))
-            .Append("</a>");
+        return (currentSortBy, sortDescPropertyName, nextSortPropertyName);
+    }
 
-        if (currentSortBy == sortPropertyName)
-            stringBuilder.Append(" <i class=\"fa fa-sort-asc\" style=\"vertical-align: middle; margin-bottom: -5px;\"></i>");
-        else if (currentSortBy == sortDescPropertyName)
-            stringBuilder.Append(" <i class=\"fa fa-sort-desc\" style=\"vertical-align: middle; margin-top: -5px;\"></i>");
+    private static string CreateUrlWithChangedSort(HttpContext context, string nextSortPropertyName)
+    {
+        var path = context.Request.Path;
+        var parameters = context.Request.Query.ToList();
 
-        return new HtmlString(stringBuilder.ToString());
+        parameters.RemoveAll(pair => pair.Key == "Page");
+        parameters.RemoveAll(pair => pair.Key == "SortBy");
+
+        if (nextSortPropertyName.IsSignificant())
+            parameters.Add(KeyValuePair.Create("SortBy", new StringValues(nextSortPropertyName)));
+
+        return QueryHelpers.AddQueryString(path, parameters);
     }
 
     public static HtmlString DisplayPagination<TModel>(
         this IHtmlHelper<TModel> htmlHelper,
+        HttpContext context,
         SortingPaginationModelBase model)
     {
         var stringBuilder = new StringBuilder();
@@ -63,9 +91,9 @@ public static class HtmlHelperExtensions
             .Append("<div style=\"margin-top: 4px;\">")
             .AppendLine();
         stringBuilder
-            .Append("<a onclick=\"changePage(")
-            .Append(pageValue - 1)
-            .Append(");\" class=\"btn btn-sm btn-outline-primary")
+            .Append("<a href=\"")
+            .Append(CreateUrlWithChangedPage(context, pageValue - 1))
+            .Append("\" class=\"btn btn-sm btn-outline-primary")
             .Append(model.HasPrevPage ? string.Empty : " disabled")
             .Append("\"><i class=\"fa fa-chevron-left\"></i> Назад</a>")
             .AppendLine();
@@ -76,9 +104,9 @@ public static class HtmlHelperExtensions
             .Append(totalPagesValue)
             .AppendLine();
         stringBuilder
-            .Append("<a onclick=\"changePage(")
-            .Append(pageValue + 1)
-            .Append(");\" class=\"btn btn-sm btn-outline-primary")
+            .Append("<a href=\"")
+            .Append(CreateUrlWithChangedPage(context, pageValue + 1))
+            .Append("\" class=\"btn btn-sm btn-outline-primary")
             .Append(model.HasNextPage ? string.Empty : " disabled")
             .Append("\">Вперед <i class=\"fa fa-chevron-right\"></i></a>")
             .AppendLine();
@@ -87,5 +115,18 @@ public static class HtmlHelperExtensions
             .AppendLine();
 
         return new HtmlString(stringBuilder.ToString());
+    }
+
+    private static string CreateUrlWithChangedPage(HttpContext context, int pageValue)
+    {
+        var path = context.Request.Path;
+        var parameters = context.Request.Query.ToList();
+
+        parameters.RemoveAll(pair => pair.Key == "Page");
+
+        if (pageValue > Constants.FirstPage)
+            parameters.Add(KeyValuePair.Create("Page", new StringValues(pageValue.ToString())));
+
+        return QueryHelpers.AddQueryString(path, parameters);
     }
 }
